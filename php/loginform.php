9e97e2b61a81a43925b0eb79580c40658b14ef38
jande081 <jande081@fiu.edu>
2016-02-17 13:33:13 -0500

    added loginform.php to php folder

--- php/loginform.php
+++ php/loginform.php
@@ -17,7 +17,7 @@
 		left: 65px;
 	}
 	</style>
-	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	<script>
 	$(function(){
 		var w = $(window).width();
